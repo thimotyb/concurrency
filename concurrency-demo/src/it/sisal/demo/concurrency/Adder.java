18c68a41769ab13f007ff3711aeb50419a9ae300
Thimoty Barbieri <thimoty@thimoty.it>
2016-12-26 15:23:57 +0100

    Tolta l'idea del parallel su stream per evitare confusioni

--- concurrency-demo/src/it/sisal/demo/concurrency/Adder.java
+++ concurrency-demo/src/it/sisal/demo/concurrency/Adder.java
@@ -33,24 +33,20 @@ public class Adder {
             @Override
             public Integer call() throws Exception {
                 /**
-                 * Simula una elaborazione parallela usando la stream api
-                 * Qui il punto sta se sia possibile suddividere il dominio
+                 * Simula una parte di elaborazione.
+                 * Qui il punto sta se sia possibile prima suddividere il dominio
                  * di calcolo impostando un approccio Map/Reduce o Fork/Join
                  * 
-                 * Qui uso una lambda di riduzione molto semplice, ma usando
-                 * una method reference si possono creare lambda più articolate
+                 * Qui la stream è usata per comodità per fare la somma, non per fare
+                 * parallelismo che invece è indotto dal fatto di stare in una Callable
                  * 
-                 * In realtà non serve usare qui uno stream per il parallelismo, perché
-                 * già l'esecuzione della call avviene in parallelo tramite 
-                 * l'Executor Service. Anzi, parallel su EE non viene in realtà
-                 * eseguito in parallelo.
-                 * 
-                 * Meglio lavorare direttamente su task Callable,
-                 * vedi esempio a call multiple su
+                 * Le Callable dovranno essere fatte in modo da lavorare su parti
+                 * del dominio da elaborare.
+                 * Vedi esempio a call multiple su
                  * https://github.com/thimotyb/java8/blob/master/java8course/src/it/corso/concurrency/Example3.java
                  * 
                  */
-                Integer mySum = elements.stream().parallel().reduce(0,  (a, b) -> a + b);
+                Integer mySum = elements.stream().reduce(0,  (a, b) -> a + b);
                 return mySum;
             }
         };
